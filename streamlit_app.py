5e558f9bd038d89e79d44d2378f6206e7510c995
tygibsoncode <tymichaelgibson@gmail.com>
2026-01-29 14:57:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Crypto Monitoring App")
+st.subheader("Find the latest cryptocurrency price updates")
+
+crypto = st.selectbox("Select a cryptocurrency:", options=[
+    "",
+    "Bitcoin",
+    "Litecoin",
+])
+
+if crypto == "Bitcoin":
+    url="https://min-api.crpytocompare.com/data/price?fsym=BTC&tsyms=USD"
+    response = requests.get(url).json()
+    btc_price = response["USD"]
+    st.success(f"The current price of {crypto} is US${btc_price:.2f}.")
+
+elif crypto == "Litecoin":
+    url="https://min-api.crpytocompare.com/data/price?fsym=BTC&tsyms=USD"
+    response = requests.get(url).json()
+    btc_price = response["USD"]
+    st.success(f"The current price of {crypto} is US${btc_price:.2f}.")
